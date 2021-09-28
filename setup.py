30d9dfe2101bc57a2ae6ae2c46675a1a5fd3e57b
Peng Lu <42014714+PENGLU-WashU@users.noreply.github.com>
2021-09-28 13:16:08 -0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -11,16 +11,13 @@ from setuptools import setup, find_packages
 setup(name='IMC_Denoise',
       packages=find_packages(),
       install_requires=[
-          "os",
           "numpy",
           "scipy",
-          "glob",
           "matplotlib",
           "jupyter",
           "sklearn",
           "tensorflow-gpu==2.2.0",
           "keras==2.3.1",
-          "tifffile",
-          "python_version==3.6"
+          "tifffile"
         ]
       )
