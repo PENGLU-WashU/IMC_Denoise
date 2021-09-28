ff6404161f0dbaf3f646f9dc2b1fddd0c2065127
Peng Lu <42014714+LUPENG7803111@users.noreply.github.com>
2021-09-27 22:44:28 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 27 22:34:57 2021
+
+@author: penglu
+"""
+
+from __future__ import absolute_import
+from setuptools import setup, find_packages
+
+setup(name='IMC_Denoise',
+      packages=find_packages()
+      )
