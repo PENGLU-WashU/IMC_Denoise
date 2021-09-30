b8a453e1f6b4f9b96ed784f2a2a4bb966c8124ae
Peng Lu <42014714+PENGLU-WashU@users.noreply.github.com>
2021-09-30 13:18:20 -0500

    Add files via upload

--- IMC_Denoise/IMC_Denoise_main/DeepSNF.py
+++ IMC_Denoise/IMC_Denoise_main/DeepSNF.py
@@ -11,9 +11,9 @@ from keras.callbacks import Callback, ModelCheckpoint, ReduceLROnPlateau
 from .DIMR import DIMR
 from .DeepSNF_model import DeepSNF_net
 from .loss_functions import create_weighted_binary_crossentropy, create_mse
-from ..DeepSNF_utils.DeepSNF_Training_DataGenerator import DeepSNF_Training_DataGenerator, manipulate_val_data
+from ..DeepSNF_utils.DeepSNF_TrainGenerator import DeepSNF_Training_DataGenerator, manipulate_val_data
 from ..Anscombe_transform_function.Anscombe_transform import Anscombe_forward
-from ..Anscombe_transform_function.3rd_party.Exact_unbiased_Anscombe_transform import Anscombe_inverse_exact_unbiased
+from ..Anscombe_transform_function.third_party.Exact_unbiased_Anscombe_transform import Anscombe_inverse_exact_unbiased
 
 class LossHistory(Callback):
     def on_train_begin(self, logs={}):
