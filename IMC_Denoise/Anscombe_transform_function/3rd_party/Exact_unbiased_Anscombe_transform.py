0e4bba5248ebc3a2f69470490ff350f86de2fe32
Peng Lu <42014714+PENGLU-WashU@users.noreply.github.com>
2021-09-30 11:13:35 -0500

    Add files via upload

---
+++ IMC_Denoise/Anscombe_transform_function/3rd_party/Exact_unbiased_Anscombe_transform.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+"""
+References:
+[1] M. Makitalo and A. Foi, "Optimal inversion of the Anscombe transformation in low-count Poisson image denoising", 
+IEEE Trans. Image Process., vol. 20, no. 1, pp. 99-109, January 2011. doi:10.1109/TIP.2010.2056693
+[2] Anscombe, F.J., "The transformation of Poisson, binomial and negative-binomial data", Biometrika, vol. 35, no. 3/4, pp. 246-254, Dec. 1948.
+
+"""
+
+import numpy as np
+from scipy.interpolate import interp1d
+import scipy.io as sio
+from .3rd_party import place_holder
+
+vector_path = place_holder.__file__
+vector_path = vector_path.replace('place_holder.py','\\')
+
+def load_Anscombe_inv_exact_params():
+    
+    """
+    Load parameters for exact unbiased inverse Anscombe transformation
+
+    """
+    Anscombe_vectors = sio.loadmat(vector_path + 'Anscombe_vectors.mat')
+    Efz = np.array(Anscombe_vectors['Efz'])
+    Ez = np.array(Anscombe_vectors['Ez'])
+    Efz = Efz[:, 0]
+    Ez = Ez[:, 0]
+
+    return Efz, Ez
+
+def Anscombe_inverse_exact_unbiased(Img_in):
+    
+    """
+    Exact unbiased inverse Anscombe transformation
+
+    """
+    Efz, Ez = load_Anscombe_inv_exact_params()
+    Img_in[Img_in < 0.0] = 0.0
+    # Img_in = Img_in * 2
+    asymptotic = (Img_in/2)**2-1/8
+    exact_inverse = interp1d(Efz,Ez,kind = 'linear', fill_value = 'extrapolate')(Img_in)
+    outside_exact_inverse_domain = Img_in > np.max(Efz)
+    exact_inverse[outside_exact_inverse_domain] = asymptotic[outside_exact_inverse_domain]
+    outside_exact_inverse_domain = Img_in < 2*np.sqrt(3/8)
+    exact_inverse[outside_exact_inverse_domain] = 0
+    
+    return exact_inverse
