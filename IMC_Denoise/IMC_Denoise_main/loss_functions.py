66989b6479e602de49c8bf6e63bf8adf6b131135
Peng Lu <42014714+PENGLU-WashU@users.noreply.github.com>
2021-10-01 09:28:31 -0500

    Add files via upload

--- IMC_Denoise/IMC_Denoise_main/loss_functions.py
+++ IMC_Denoise/IMC_Denoise_main/loss_functions.py
@@ -5,9 +5,9 @@ import tensorflow as tf
 def create_weighted_binary_crossentropy():
   def weighted_ce(y_true, y_pred):
     target, mask = tf.split(y_true, 2, axis = -1)
-    loss1 = tf.multiply(target * mask, tf.math.log(y_pred * mask + 1e-15))
-    loss2 = tf.multiply(1.0 - target * mask, tf.math.log(1.0 - y_pred * mask + 1e-15))
-    bce = -(loss1 + loss2)  
+    loss1 = tf.multiply(target, tf.math.log(y_pred + 1e-15))
+    loss2 = tf.multiply(1.0 - target, tf.math.log(1.0 - y_pred + 1e-15))
+    bce = tf.multiply(-(loss1 + loss2), mask)
     return tf.reduce_sum(bce)/ tf.reduce_sum(mask)
   return weighted_ce
   
