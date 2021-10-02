47976099cd222b552e88ad50ce87b154891a08e9
Peng Lu <42014714+PENGLU-WashU@users.noreply.github.com>
2021-10-01 21:48:48 -0500

    Update DeepSNF_TrainGenerator.py

--- IMC_Denoise/DeepSNF_utils/DeepSNF_TrainGenerator.py
+++ IMC_Denoise/DeepSNF_utils/DeepSNF_TrainGenerator.py
@@ -2,7 +2,7 @@
 
 """
 The codes for generating training and validation data in this file are modified from Noise2Void: https://github.com/juglab/n2v.
-We assemble and implement the modified codes in our IMC_Denoise package.
+We assemble and implement the modified codes in our IMC_Denoise package. Please cite the references below if using this software.
 
 Reference:
 [1] Krull, Alexander, Tim-Oliver Buchholz, and Florian Jug. "Noise2void-learning denoising from single noisy images." 
