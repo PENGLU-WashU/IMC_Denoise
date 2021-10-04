1f821b067de2250cc9422c2574495506e828e95c
Peng Lu <42014714+PENGLU-WashU@users.noreply.github.com>
2021-10-04 16:07:00 -0500

    Add files via upload

--- scripts/Data_generation_DeepSNF_script.py
+++ scripts/Data_generation_DeepSNF_script.py
@@ -9,17 +9,17 @@ from IMC_Denoise.DeepSNF_utils.DeepSNF_DataGenerator import DeepSNF_DataGenerato
 import argparse
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("--channel_name", help = "channel used to generate training set, e.g. 141Pr")
-parser.add_argument("--Raw_directory", help = "The directory which contained raw IMC images used to generate training set")
+parser.add_argument("--channel_name", help = "channel used to generate training set, e.g. 141Pr", type = str)
+parser.add_argument("--Raw_directory", help = "The directory which contained raw IMC images used to generate training set", type = str)
 parser.add_argument("--Save_directory", help = "The directory which saves the generated training set. If None, the generated \
-                    training set will be saved in the default directory", default = None)
+                    training set will be saved in the default directory", default = None, type = str)
 parser.add_argument("--n_neighbours", help = "DIMR algorithm parameter", default = 4, type = int)
-parser.add_argument("--n_lambda", help = "DIMR algorithm parameter", default = 5)
-parser.add_argument("--slide_window_size", help = "DIMR algorithm parameter", default = 3)
+parser.add_argument("--n_lambda", help = "DIMR algorithm parameter", default = 5, type = float)
+parser.add_argument("--slide_window_size", help = "DIMR algorithm parameter", default = 3, type = int)
 
 args = parser.parse_args()
 print(args)
-
+print(args.slide_window_size+1)
 DataGenerator = DeepSNF_DataGenerator(channel_name = args.channel_name, n_neighbours = args.n_neighbours, \
                                       n_lambda = args.n_lambda, window_size = args.slide_window_size)
 generated_patches = DataGenerator.generate_patches_from_directory(load_directory = args.Raw_directory)
