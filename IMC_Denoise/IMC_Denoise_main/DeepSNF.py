4a72694b73d06b7dcfc0c1e35b5ab301721cdb86
Peng Lu <42014714+PENGLU-WashU@users.noreply.github.com>
2021-10-04 20:31:07 -0500

    Add files via upload

--- IMC_Denoise/IMC_Denoise_main/DeepSNF.py
+++ IMC_Denoise/IMC_Denoise_main/DeepSNF.py
@@ -17,15 +17,13 @@ from ..Anscombe_transform.Anscombe_transform_functions import Anscombe_forward, 
 import tensorflow as tf
 gpus = tf.config.list_physical_devices('GPU')
 if gpus:
-  try:
-    # Currently, memory growth needs to be the same across GPUs
-    for gpu in gpus:
-      tf.config.experimental.set_memory_growth(gpu, True)
-    logical_gpus = tf.config.list_logical_devices('GPU')
-    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-  except RuntimeError as e:
-    # Memory growth must be set before GPUs have been initialized
-    print(e)
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)
 
 class LossHistory(Callback):
     def on_train_begin(self, logs={}):
