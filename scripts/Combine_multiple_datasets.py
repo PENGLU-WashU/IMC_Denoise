60f57fe517126b4b25ccaf4be01ce4645b6936d6
Peng Lu <42014714+PENGLU-WashU@users.noreply.github.com>
2022-05-01 22:20:29 -0500

    Add files via upload

--- scripts/Combine_multiple_datasets.py
+++ scripts/Combine_multiple_datasets.py
@@ -8,13 +8,13 @@ from os.path import isfile, join
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 parser.add_argument("--load_directory", help = "the folder of the generated IMC training sets", type = str)
-parser.add_argument("--saved_directory", help = "the folder to save the combined IMC training set", type = str)
+parser.add_argument("--save_directory", help = "the folder to save the combined IMC training set", type = str)
 parser.add_argument("--saved_training_set_name", help = "the name of the saved IMC training set", default = 'combined_training_set.npz', type = str)
                     
 args = parser.parse_args()
 print(args)
 
-if not args.saved_training_set_name.endswith(".npz"):
+if not args.save_training_set_name.endswith(".npz"):
     print('The saved training set name must end with .npz!')
     quit()
 
@@ -31,7 +31,7 @@ for ii in range(len(dataset_list)):
         combined_dataset = np.concatenate((combined_dataset, cur_dataset), axis = 0)
         
 np.random.shuffle(combined_dataset)
-saved_name = args.saved_directory + args.saved_training_set_name
+saved_name = args.save_directory + args.saved_training_set_name
 np.savez(saved_name, patches = combined_dataset)
 print('The generated training set with shape of {} is saved as {}.'.format(combined_dataset.shape, saved_name))
 
