416c286b8680c897baabf864a4985660912dc155
Peng Lu <42014714+PENGLU-WashU@users.noreply.github.com>
2022-11-20 21:21:48 -0600

    Update DeepSNiF_DataGenerator.py

--- IMC_Denoise/DeepSNiF_utils/DeepSNiF_DataGenerator.py
+++ IMC_Denoise/DeepSNiF_utils/DeepSNiF_DataGenerator.py
@@ -177,7 +177,7 @@ class DeepSNiF_DataGenerator():
                     Img_collect.append(Img_read)
                     break
         
-        print('\n' + 'Image data loaded completed!')
+        print('\n' + 'Image data loading completed!')
         if not Img_collect:
             print('No such channels! Please check the channel name again!')
             return
