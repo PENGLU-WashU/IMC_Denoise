ddf3444681dc684d261184c34a0b0d7452bda64e
Lukas Hatscher <lukashat@vpn509-107.rzuser.uni-heidelberg.de>
2024-04-10 18:35:03 +0200

    changed the CLI script again for better folder handling

--- Jupyter_Notebook_examples/IMC_Denoise_Steinbock.py
+++ Jupyter_Notebook_examples/IMC_Denoise_Steinbock.py
@@ -1,70 +1,65 @@
-### This script is to integrate DMIR / DeepSNIF analysis from the IMC_denoise package into the steinbock (https://bodenmillergroup.github.io/steinbock/) workflow
+# %%
+### This example notebook is to integrate DIMR / DeepSNiF analysis from the IMC_denoise package into the steinbock (https://bodenmillergroup.github.io/steinbock/) workflow
 ### Step of steinbock pipeline: after the conversion of mcd into .tiff files 
 
-### Edited from the example DeepSNiF train/run jupyter notebook script in IMC_denoise
+### Edited by Ben Caiello from the example DeepSNiF train/run jupyter notebook script in IMC_denoise
 import os
 import shutil
 import numpy as np
-import matplotlib.pyplot as plt
 import tifffile as tp
 from IMC_Denoise.IMC_Denoise_main.DIMR import DIMR
 from IMC_Denoise.IMC_Denoise_main.DeepSNiF import DeepSNiF
 from IMC_Denoise.DeepSNiF_utils.DeepSNiF_DataGenerator import DeepSNiF_DataGenerator
 import argparse
+import tempfile
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-t", "--train", type=str, help="Path to the training directory")
 parser.add_argument("-i", "--input", type=str, help="Path to the input directory where raw multichannel tiffs are located")
-parser.add_argument("-p", "--pre_denoised", type=str, default=None, help="Optional: Path to the pre-denoised directory where original raw files will be backed up")
+parser.add_argument("-o", "--output", type=str, help="Path to the directory where denoised images will be saved. If not specified, denoised images will overwrite the original images in the input directory.")
 parser.add_argument("-c", "--channels", type=str, default=None, help="Comma-separated list of channels to denoise (e.g., '1,2,3'). If not provided, all channels will be used.")
 parser.add_argument("-b", "--batch_size", type=int, default=128, help="Batch size for training of DeepSNIF, default is 128")
 parser.add_argument("-e", "--epochs", type=int, default=100, help="Number of Epochs for training, default is 100")
-parser.add_argument("-l", "--learning_rate", type=int, default=1e-3, help="Initial learning rate, default ist 1e-3")
+parser.add_argument("-l", "--learning_rate", type=float, default=1e-3, help="Initial learning rate, default ist 1e-3")
 args = parser.parse_args()
 
 if 'generated_patches' in globals():
     del generated_patches
 
 train_directory = args.train 
-Raw_directory = args.input 
-pre_denoised_directory = args.pre_denoised
+Raw_directory = args.input
+output_directory = args.output
 
-if not os.path.exists(train_directory) or not os.path.exists(Raw_directory):
-    raise FileNotFoundError("Training or raw directory does not exist.")
-
-if pre_denoised_directory:
-    if not os.path.exists(pre_denoised_directory):
-        os.makedirs(pre_denoised_directory)
-    for filename in os.listdir(Raw_directory):
-        shutil.copy(os.path.join(Raw_directory, filename), os.path.join(pre_denoised_directory, filename))
-    print(f"Original raw files have been backed up to {pre_denoised_directory}")
-
-output_directory = Raw_directory
-
+#### Choose what channels you want to denoise:
 if args.channels:
     channel_names = [int(ch.strip()) for ch in args.channels.split(',')]
 else:
-    # Dynamically determine all channels if none are specified
-    print('No channels specified! Will attempt to denoise all channels in the provided .tiffs')
-    first_tiff_path = os.path.join(Raw_directory, os.listdir(Raw_directory)[0])
-    with tp.TiffFile(first_tiff_path) as tif:
-        channel_number = len(tif.pages)
-        channel_names = list(range(channel_number))  
-### Step 0.1: edit IMC_denoise so that it will ingest multi-channle tiffs directly from the directory, instead of requiring the directory structure of the original IMC_denoise package
-# Edits in this file: ...\IMC_Denoise\DeepSNIF_utils\DeepSniF_DataGenerator.py
-#display()
+    channel_names = []
 
-## Step 0.2: set up a function that integrates all the training functions of IMC_denoise
+# Check if the output directory is the same as the raw directory
+if Raw_directory == output_directory:
+    # Create a temporary directory to store intermediate outputs
+    temp_output_dir = tempfile.mkdtemp()
+else:
+    temp_output_dir = output_directory
+
+#if Raw_directory == output_directory:    
+#    if os.path.isdir(Raw_directory + "_pre_denoise") == False:
+#        shutil.copytree(Raw_directory,(Raw_directory + "_pre_denoise"))
+#    else:
+#        print('img_pre_denoise folder already exists, will not copy current img directory')
+        
+## Step 0.1: set up a function that integrates all the training functions of IMC_denoise
 # Just done here so that the code for iterating through the channels is simpler
-# DO note the -- run_type = 'multi_channel_tiff' --  attribute in the DataGenerator call: this is what allows the ingestion of multi-channel .tiffs by the program 
-# Without specifying run_type (by specifying runtype = 'single_channel_tiff'), IMC Denoise should default to reading the official, single-channel .tiff format
-def DeepSNIF_train(channel_name, n_neighbours = 4, n_iter = 3, window_size = 3, train_epoches = 100, train_initial_lr = 1e-3, 
+# DO note the -- run_type = 'multi_channel_tiff' --  attribute in the DataGenerator call: this is what allows the ingestion of multi-channel .tiffs
+# Without specifying run_type, IMC Denoise should default to reading the official, single-channel .tiff format
+def DeepSNiF_train(channel_name, n_neighbours = 4, n_iter = 3, window_size = 3, train_epoches = 25, train_initial_lr = 1e-3, 
                   train_batch_size = 128, pixel_mask_percent = 0.2, val_set_percent = 0.15, loss_function = "I_divergence",
                   weights_name = None, loss_name = None, weights_save_directory = None, lambda_HF = 3e-6):
     '''
     This function merges the training functions of IMC_denoise for simplicity of the script here, particularly with most/all default setting for hyperparamters.
-    If you want more control / want to adjust many of the hyperparameters, it may be better to split this function into the original pieces like original IMC_Denoise_Train_and_Predict notebook. 
-    This function is also set to work with multi-channel .tiffs, not single-channel, but that could be changed by editing the runtype = 'multi_channel_tiff' argument in the DataGenerator call. 
+    If you want more control / want to adjust many of the hyperparameters, it may be better to split this function into its original pieces. 
+    This function is also set to work only with multi-channel .tiffs, but that can be edited in the DataGenerator call.
     '''
     DataGenerator = DeepSNiF_DataGenerator(run_type = 'multi_channel_tiff', channel_name = channel_name, ratio_thresh = 0.8,
                                            patch_row_size = 64, patch_col_size = 64, row_step = 60, col_step = 60,
@@ -88,18 +83,33 @@ def DeepSNIF_train(channel_name, n_neighbours = 4, n_iter = 3, window_size = 3, 
 
 
 # Step 1: iterate through the channels, training  and then running for each image
+ # This code block makes it so that if no channel names are specified, all channels are run! Depending on how many channels / how long the training -- that would take a very long time
+if len(channel_names) < 1:
+    print('No channels specified! Attempting to denoise all channels in provided .tiffs...')
+    try:
+        with tp.TiffFile(os.path.join(Raw_directory, os.listdir(Raw_directory)[0])) as tif:
+            channel_names = list(range(len(tif.pages)))
+    except Exception as e:
+        print(f"Error reading TIFF file to determine channels: {e}")
+        exit(1)
+
+# This loop iterates through the list of channels and performs DIMR + DeepSNiF on each:
 for i in channel_names:
-    n_neighbours, n_iter, window_size, train_loss, val_loss, deepsnif = DeepSNIF_train(i)
+    n_neighbours, n_iter, window_size, train_loss, val_loss, deepsnif = DeepSNiF_train(i)
     for img in os.listdir(Raw_directory):
         img_path = os.path.join(Raw_directory, img)
-        if not img_path.lower().endswith('.tif'):
-            continue  # Skip non-TIFF files
         Img_raw = tp.TiffFile(img_path).pages[i].asarray()
-        Img_DIMR_DeepSNiF = deepsnif.perform_IMC_Denoise(Img_raw, n_neighbours=n_neighbours, n_iter=n_iter, window_size=window_size)
+        Img_DIMR_DeepSNiF = deepsnif.perform_IMC_Denoise(Img_raw, n_neighbours = n_neighbours, n_iter = n_iter, window_size = window_size)
         numpy_tiff = tp.imread(img_path)
         numpy_tiff[i] = Img_DIMR_DeepSNiF
-        tp.imwrite(os.path.join(output_directory, img), numpy_tiff, photometric='minisblack')
+        tp.imwrite(os.path.join(temp_output_dir, img), numpy_tiff, photometric='minisblack')
 
+# If a temporary directory was used, move the processed files back to the original directory
+if Raw_directory == output_directory:
+    for img in os.listdir(temp_output_dir):
+        shutil.move(os.path.join(temp_output_dir, img), os.path.join(Raw_directory, img))
+    # Remove the temporary directory
+    shutil.rmtree(temp_output_dir)
 # Steinbock should now be able to seemlessly work with the denoised files
 
 
